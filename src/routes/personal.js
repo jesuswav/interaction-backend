8df2f30572a3739c83e520e90fa43dcb21f3db69
jesuswav <garciajesse2003@gmail.com>
2024-08-22 01:03:22 -0600

    [add] Add group by teams search response

--- src/routes/personal.js
+++ src/routes/personal.js
@@ -134,64 +134,103 @@ router.post('/search', (req, res) => {
           FROM Posts
           LEFT JOIN PostLikesList ON Posts.post_id = PostLikesList.post_id
           WHERE Posts.post_id IN (?)
-          GROUP BY Posts.post_id;
+          GROUP BY Posts.post_id
         `
 
-        connection.query(likesQuery, [posts_id], (err, postsWithLikes) => {
-          if (err) {
-            console.log(err)
-          }
-
-          results?.forEach((row) => {
-            //   WHERE
-            // DATE(Posts.register_date) = ?
-
-            // [date],
-
-            if (!usersWithPublications[row.personal_id]) {
-              // Si no existe, crea una nueva entrada para el usuario
-              usersWithPublications[row.personal_id] = {
-                personal_id: row.personal_id,
-                personal_name: row.personal_name,
-                personal_team: row.team_name,
-                team_color: row.team_color,
-                posts: [],
-              }
+        // Comprobar si hay IDs para evitar lanzar un error SQL
+        if (posts_id.length > 0) {
+          connection.query(likesQuery, [posts_id], (err, postsWithLikes) => {
+            if (err) {
+              console.log(err)
             }
 
-            // Añade la publicación actual al array de publicaciones del usuario
-            usersWithPublications[row.personal_id].posts.push({
-              post_id: row.post_id,
-              post_description: row.post_description,
-              checked: row.checked,
-              unique_post_id: row.unique_post_id,
-              likes: '',
-            })
+            results?.forEach((row) => {
+              //   WHERE
+              // DATE(Posts.register_date) = ?
 
-            const userWithPublicationsArray = Object.values(
-              usersWithPublications
-            )
+              // [date],
 
-            // Agregamos los likes a los usuarios con publicaciones y a las publicaciones respectivas
-            userWithPublicationsArray.forEach((item) => {
-              item.posts.forEach((item_post) => {
-                postsWithLikes.forEach((post_id_item) => {
-                  if (item_post.post_id === post_id_item.post_id) {
-                    usersWithPublications[item.personal_id].posts.forEach(
-                      (final_post) => {
-                        if (final_post.post_id === post_id_item.post_id) {
-                          final_post.likes = post_id_item.like_names
+              if (!usersWithPublications[row.personal_id]) {
+                // Si no existe, crea una nueva entrada para el usuario
+                usersWithPublications[row.personal_id] = {
+                  personal_id: row.personal_id,
+                  personal_name: row.personal_name,
+                  personal_team: row.team_name,
+                  team_color: row.team_color,
+                  posts: [],
+                }
+              }
+
+              // Añade la publicación actual al array de publicaciones del usuario
+              usersWithPublications[row.personal_id].posts.push({
+                post_id: row.post_id,
+                post_description: row.post_description,
+                checked: row.checked,
+                unique_post_id: row.unique_post_id,
+                likes: '',
+              })
+
+              const userWithPublicationsArray = Object.values(
+                usersWithPublications
+              )
+
+              // Agregamos los likes a los usuarios con publicaciones y a las publicaciones respectivas
+              userWithPublicationsArray.forEach((item) => {
+                item.posts.forEach((item_post) => {
+                  postsWithLikes.forEach((post_id_item) => {
+                    if (item_post.post_id === post_id_item.post_id) {
+                      usersWithPublications[item.personal_id].posts.forEach(
+                        (final_post) => {
+                          if (final_post.post_id === post_id_item.post_id) {
+                            final_post.likes = post_id_item.like_names
+                          }
                         }
-                      }
-                    )
-                  }
+                      )
+                    }
+                  })
                 })
               })
             })
-          })
 
-          res.json(Object.values(usersWithPublications))
-        })
+            //Obtener los resultados clasificados en base al equipo que tienen
+
+            const teams = {}
+
+            // Recorrer el objeto original
+            Object.values(usersWithPublications).forEach((personal) => {
+              const {
+                personal_team,
+                team_color,
+                personal_id,
+                personal_name,
+                posts,
+              } = personal
+
+              // Si el equipo no existe en el objeto teams, lo creamos
+              if (!teams[personal_team]) {
+                teams[personal_team] = {
+                  team_name: personal_team,
+                  team_color: team_color,
+                  members: [],
+                }
+              }
+
+              // Agregar el personal al equipo correspondiente
+              teams[personal_team].members.push({
+                personal_id,
+                personal_name,
+                posts,
+              })
+            })
+
+            // Convertir el objeto teams en un array
+            const groupedTeams = Object.values(teams)
+
+            res.json(Object.values(groupedTeams))
+          })
+        } else {
+          console.log('No hay IDs disponibles.')
+        }
       }
     )
   })
